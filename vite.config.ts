5bddb09adff6ad381c9f8695b5760c538561f6cc
amirazemi04 <amirazemi04@gmail.com>
2026-02-07 20:22:29 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ['firebase/app', 'firebase/firestore'],
     exclude: ['lucide-react'],
   },
 });
