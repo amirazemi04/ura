322d41756013581a99962d99b95767a3ad30b8a9
amirazemi04 <amirazemi04@gmail.com>
2026-02-08 10:11:35 +0100

    Updated generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,31 +1,26 @@
-const { SitemapStream, streamToPromise } = require('sitemap');
-const { createWriteStream } = require('fs');
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
 
-// Domaini yt
 const sitemap = new SitemapStream({ hostname: 'https://ansambli-ura.ch' });
 
-// Rrugët e faqes (nga App.js që më dërgove)
 const pages = [
-  '/', 
-  '/groups', 
-  '/join-us', 
+  '/',
+  '/groups',
+  '/join-us',
   '/sponsor-contact',
-  '/gallery', 
-  '/about-us', 
+  '/gallery',
+  '/about-us',
   '/sponsors',
-  '/privacy-policy', 
+  '/privacy-policy',
   '/terms-of-service'
 ];
 
-// Shto secilën faqe në sitemap
 pages.forEach(page => {
   sitemap.write({ url: page, changefreq: 'weekly', priority: 0.8 });
 });
 
-// Mbyll stream-in
 sitemap.end();
 
-// Shkruaj sitemap.xml te public/
 streamToPromise(sitemap).then(sm =>
   createWriteStream('./public/sitemap.xml').write(sm.toString())
 );
